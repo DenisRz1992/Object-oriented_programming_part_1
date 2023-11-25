b7b189ccc6c5a5c9239fdf83292542f5901eb162
User <drazinkin12@gmail.com>
2023-11-25 23:06:03 +0300

    1 committ

---
+++ src/Hogwarts/Slytherin.java
@@ -0,0 +1,53 @@
+package Hogwarts;
+
+public class Slytherin extends Hogwarts {
+    private int cunning;
+    private int determination;
+    private int ambition;
+    private int resourcefulness;
+    private int lustForPower;
+
+    public Slytherin( String name, int skillMagic, int trangression, int cunning, int determination, int ambition, int resourcefulness, int lustForPower) {
+        super( name, skillMagic, trangression);
+        this.cunning = cunning;
+        this.determination = determination;
+        this.ambition = ambition;
+        this.resourcefulness = resourcefulness;
+        this.lustForPower = lustForPower;
+    }
+
+    public int getCunning () { return cunning;}
+
+    public int getDetermination() {
+        return determination;
+    }
+
+    public int getAmbition() {
+        return ambition;
+    }
+
+    public int getResourcefulness() {
+        return resourcefulness;
+    }
+
+    public int getLustForPower() {
+        return lustForPower;
+    }
+
+    public String toString () {
+        return super.toString() + " обладает хтростью, " + cunning + " , решительностью " + determination + " , амбициозностью " + ambition +
+                " , находчивостью " + resourcefulness + " и жаждой власти " + lustForPower;
+    }
+
+    public void compareStudentsSlytherin(Slytherin student) {
+
+        if (this.getCunning()  + this.getDetermination() + this.getAmbition() + this.getResourcefulness() + this.getLustForPower() >
+        student.getCunning() + student.getDetermination() + student.getAmbition() + student.getResourcefulness() + student.getLustForPower()) {
+            System.out.println("Слизериновец " + this.getName() + " cильнее, чем " + student.getName());
+        } else if (this.getCunning() < student.getCunning()) {
+            System.out.println("Слизериновец " + student.getName() + " сильнее, чем " + this.getName());
+        } else {
+            System.out.println("Слизериновец " + this.getName() + " и " + student.getName() + " одинаково сильны");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import Hogwarts.Slytherin;
+import Hogwarts.Hogwarts;
+import Hogwarts.Hufflepuff;
+import Hogwarts.Ravenclaw;
+import Hogwarts.Gryffindor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Slytherin Draco = new Slytherin("Драго", 8, 86, 45, 78, 78, 89, 67);
+        Slytherin Goil = new Slytherin("Гойл", 0,23,56,21,67,3,67);
+        Gryffindor Ron = new Gryffindor("Рон", 0, 1, 3, 6, 5);
+        Gryffindor Germiona = new Gryffindor("Гермиона", 3,4,5,5,6);
+        Germiona.compareStudents(Ron);
+        Draco.compareStudentsSlytherin(Goil);
+        Draco.compareStudents(Goil);
+        Hufflepuff hufflepuff = new Hufflepuff( "Захария", 18, 45, 3, 7, 56);
+        Hogwarts [] hogwartsStudents = new Hogwarts[] { hufflepuff, Draco, Goil};
+        for (Hogwarts student : hogwartsStudents) {
+        }
     }
 }
